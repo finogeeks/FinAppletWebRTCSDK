12c6a8034e65f5c86d03e3b6e4f90c9cb8891518
developer <developer@finogeeks.com>
2021-12-14 11:44:57 +0800

    version++

---
+++ FinAppletWebRTC.framework/Headers/FATWebRTCComponent.h
@@ -0,0 +1,18 @@
+//
+//  FATWebRTCComponent.h
+//  FinAppletWebRTC
+//
+//  Created by 王滔 on 2021/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FATWebRTCComponent : NSObject
+
++ (void)registerComponent;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FinAppletWebRTC.framework/Headers/FinAppletWebRTC.h
@@ -0,0 +1,19 @@
+//
+//  FinAppletWebRTC.h
+//  FinAppletWebRTC
+//
+//  Created by 王滔 on 2021/11/3.
+//
+
+#import <Foundation/Foundation.h>
+#import "FATWebRTCComponent.h"
+
+//! Project version number for FinAppletWebRTC.
+FOUNDATION_EXPORT double FinAppletWebRTCVersionNumber;
+
+//! Project version string for FinAppletWebRTC.
+FOUNDATION_EXPORT const unsigned char FinAppletWebRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinAppletWebRTC/PublicHeader.h>
+
+
